07574579182ada37450e804e7befadfc7763647b
janusz080808 <44392860+janusz080808@users.noreply.github.com>
2018-12-10 20:21:05 +0100

    Update janusz080808.py

--- 2018_2019/8Ca/Pierwszy_program/janusz080808.py
+++ 2018_2019/8Ca/Pierwszy_program/janusz080808.py
@@ -1,1 +1,12 @@
-
+print("program obliczajacy wynik dzielenia")
+dzielna = float(input("podaj dzielna")) 
+dzielnik = float(input("podaj dzielnik"))
+dana = 0
+while dzielnik == dana:
+    print("nie mozna dzielic przez", dana,".")
+    dzielnik = float(input("podaj dzielnik jeszcze raz:" ))
+wynik = (dzielna/dzielnik,)
+reszta = (dzielna % dzielnik)
+reszta1 = (dzielna - reszta) / dzielnik
+print("wynik to:", wynik, "a wynik z reszta to", reszta1, "i", reszta)
+   
